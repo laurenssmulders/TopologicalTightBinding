12f630e049abf1add0e8fca1c2850e8f51f5fe0d
Laurens Smulders <64740743+laurenssmulders@users.noreply.github.com>
2023-11-02 22:36:25 +0000

    Gauge visualisation

--- tight_binding/solver.py
+++ tight_binding/solver.py
@@ -18,8 +18,6 @@ class bandstructure2D:
         self.b_1, self.b_2 = compute_reciprocal_lattice_vectors_2D(a_1,a_2)
         self.bands = bands
         self.n = n
-        print(self.b_1)
-        print(self.b_2)
 
     def compute_bandstructure(self):
         """Computes the bandstructure for one reciprocal unit cell."""
@@ -29,7 +27,6 @@ class bandstructure2D:
         alpha_1, alpha_2 = np.meshgrid(alpha_1, alpha_2, indexing='ij')
         kx = alpha_1*self.b_1[0] + alpha_2*self.b_2[0]
         ky = alpha_1*self.b_1[1] + alpha_2*self.b_2[1]
-        print(np.sum(kx < 0))
         energies = np.zeros(alpha_1.shape + (self.bands,), dtype='float')
         blochvectors = np.zeros(alpha_1.shape + (self.bands,self.bands), 
                                 dtype='float')
@@ -43,9 +40,9 @@ class bandstructure2D:
                 ind = np.argsort(eigenvalues) #sorting the energies
                 eigenvalues = eigenvalues[ind]
                 eigenvectors = eigenvectors[:,ind]
-                for eigenvalue in range(len(eigenvalues)):
-                    energies[i,j,eigenvalue] = eigenvalues[eigenvalue]
-                    blochvectors[i,j,eigenvalue] = eigenvectors[:,eigenvalue]
+                for band in range(len(eigenvalues)):
+                    energies[i,j,band] = eigenvalues[band]
+                    blochvectors[i,j,band] = eigenvectors[:,band]
         
         # Saving as class properties
         self.alpha_1 = alpha_1
@@ -55,6 +52,41 @@ class bandstructure2D:
         self.energies = energies
         self.blochvectors = blochvectors
     
+    def plot_gauge(self, band_index, save):
+        """Visualises the gauge continuity over the BZ"""
+        gauge = np.zeros(self.alpha_1.shape, dtype='float')
+        for i in range(gauge.shape[0]):
+            for j in range(gauge.shape[1]):
+                gauge_indicator = (
+                    np.inner(self.blochvectors[i,j,band_index], 
+                            self.blochvectors[(i+1) % self.blochvectors.shape[0], 
+                                              j, 
+                                              band_index])
+                    + np.inner(self.blochvectors[i,j,band_index], 
+                            self.blochvectors[(i-1) % self.blochvectors.shape[0], 
+                                              j, 
+                                              band_index])
+                    + np.inner(self.blochvectors[i,j,band_index], 
+                            self.blochvectors[i, 
+                                              (j+1) % self.blochvectors.shape[1], 
+                                              band_index])
+                    + np.inner(self.blochvectors[i,j,band_index], 
+                            self.blochvectors[i, 
+                                              (j-1) % self.blochvectors.shape[1], 
+                                              band_index])) / 4
+                gauge[i,j] = gauge_indicator
+        
+                    
+        fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+        # Plot the surface.
+        surf = ax.plot_surface(self.kx, self.ky, gauge, cmap=cm.coolwarm,
+                            linewidth=0, antialiased=True)
+        plt.savefig(save)
+        plt.show()        
+                                              
+
+
+
     def plot_bandstructure(self, save):
         "Plots and saves the bandstructure." 
         E = np.transpose(self.energies, (2,0,1))
@@ -62,11 +94,11 @@ class bandstructure2D:
         fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
         # Plot the surface.
         surf1 = ax.plot_surface(self.kx, self.ky, E[0], cmap=cm.coolwarm,
-                            linewidth=0, antialiased=False)
+                            linewidth=0, antialiased=True)
         surf2 = ax.plot_surface(self.kx, self.ky, E[1], cmap=cm.coolwarm,
-                            linewidth=0, antialiased=False)
+                            linewidth=0, antialiased=True)
         surf3 = ax.plot_surface(self.kx, self.ky, E[2], cmap=cm.coolwarm,
-                            linewidth=0, antialiased=False)
+                            linewidth=0, antialiased=True)
         plt.savefig(save)
         plt.show()
         
