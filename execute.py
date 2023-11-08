7175d61d69f4a4ad6cd017265e6adbde8052f329
Laurens Smulders <64740743+laurenssmulders@users.noreply.github.com>
2023-11-08 18:14:33 +0000

    Figures

--- execute.py
+++ execute.py
@@ -5,15 +5,15 @@ import numpy as np
 def A(t):
     return np.array([2*np.cos(6*t), 0])
 
-H = driven_bloch_hamiltonian_kagome(1,1,1,1,1,A)
+H = driven_bloch_hamiltonian_kagome(-0.5,1,-0.5,-1,1,A)
 bs = driven_bandstructure2D(H, 
                             2*np.pi / 6, 
                             0.01, 
                             np.array([1,0]),
                             np.array([0.5,0.5*3**0.5]), 
-                            50, 
-                            3, 
-                            1)
+                            50,
+                            3,
+                            -1.25*np.pi)
 
 bs.compute_bandstructure()
-bs.plot_bandstructure('driven_bandstructure2.png')
+bs.plot_bandstructure('test.png', -1.25*np.pi, 1.25*np.pi, -1.25*np.pi, 1.25*np.pi)
