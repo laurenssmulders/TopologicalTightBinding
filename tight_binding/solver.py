b2b395866630dccf14af4294acb9fb3b660e8412
Laurens Smulders <64740743+laurenssmulders@users.noreply.github.com>
2023-11-15 18:35:30 +0000

    Set gauge and allowed for complex blochvectors

--- tight_binding/solver.py
+++ tight_binding/solver.py
@@ -30,7 +30,7 @@ class static_bandstructure2D:
         ky = alpha_1*self.b_1[1] + alpha_2*self.b_2[1]
         energies = np.zeros(alpha_1.shape + (self.bands,), dtype='float')
         blochvectors = np.zeros(alpha_1.shape + (self.bands,self.bands), 
-                                dtype='float')
+                                dtype='complex')
         
         # Computing the right values for these grids
         for i in range(alpha_1.shape[0]):
@@ -38,6 +38,7 @@ class static_bandstructure2D:
                 k = np.array([kx[i,j], ky[i,j]])
                 H = self.hamiltonian(k)
                 eigenvalues, eigenvectors = np.linalg.eig(H)
+                eigenvalues = np.real(eigenvalues) # H is hermitian
                 ind = np.argsort(eigenvalues) #sorting the energies
                 eigenvalues = eigenvalues[ind]
                 eigenvectors = eigenvectors[:,ind]
@@ -53,53 +54,110 @@ class static_bandstructure2D:
         self.energies = energies
         self.blochvectors = blochvectors
     
-    def plot_gauge(self, band_index, save):
-        """Visualises the gauge continuity over the BZ."""
-        gauge = np.zeros(self.alpha_1.shape, dtype='float')
-        for i in range(gauge.shape[0]):
-            for j in range(gauge.shape[1]):
-                gauge_indicator = (
-                    np.inner(self.blochvectors[i,j,band_index], 
-                            self.blochvectors[(i+1) % self.blochvectors.shape[0], 
-                                              j, 
-                                              band_index])
-                    + np.inner(self.blochvectors[i,j,band_index], 
-                            self.blochvectors[(i-1) % self.blochvectors.shape[0], 
-                                              j, 
-                                              band_index])
-                    + np.inner(self.blochvectors[i,j,band_index], 
-                            self.blochvectors[i, 
-                                              (j+1) % self.blochvectors.shape[1], 
-                                              band_index])
-                    + np.inner(self.blochvectors[i,j,band_index], 
-                            self.blochvectors[i, 
-                                              (j-1) % self.blochvectors.shape[1], 
-                                              band_index])) / 4
-                gauge[i,j] = gauge_indicator
+    def set_gauge(self):
+        for band in range(self.bands):
+            for i in range(self.blochvectors.shape[0]):
+                for j in range(self.blochvectors.shape[1]):
+                    if i == 0 and j == 0:
+                        pass
+                    elif i==0 and np.inner(self.blochvectors[i,j,band], 
+                                           self.blochvectors[i,j-1,band]) < 0:
+                        self.blochvectors[i,j,band] = -self.blochvectors[i,j,
+                                                                         band]
+                    elif np.inner(self.blochvectors[i,j,band], 
+                                  self.blochvectors[i-1,j,band]) < 0:
+                        self.blochvectors[i,j,band] = -self.blochvectors[i,j,
+                                                                         band]
+    
+    def plot_blochvectors(self,
+                          band,
+                          component,
+                          save,
+                          kxmin=-np.pi,
+                          kxmax=np.pi,
+                          kymin=-np.pi,
+                          kymax=np.pi):
+        "Plots one component of the blochvectors."
+        # Getting the correct region
+        span = False
+        reciprocal_vectors = 0
+        while not span:
+            reciprocal_vectors += 1
+            beta_1 = np.linspace(-reciprocal_vectors,
+                                 reciprocal_vectors,
+                                 2*reciprocal_vectors*self.n)
+            beta_2 = np.linspace(-reciprocal_vectors,
+                                 reciprocal_vectors,
+                                 2*reciprocal_vectors*self.n)
+            beta_1, beta_2 = np.meshgrid(beta_1, beta_2, indexing='ij')
+            kx = beta_1*self.b_1[0] + beta_2*self.b_2[0]
+            ky = beta_1*self.b_1[1] + beta_2*self.b_2[1]
+            span = ((np.min(kx) < kxmin) and (np.max(kx) > kxmax) 
+                    and (np.min(ky) < kymin) and (np.max(ky) > kymax))
+            
+        # Specifying the indices of the required vectors in self.blochvectors
+        i = (self.n*(beta_1%1)).astype(int)
+        j = (self.n*(beta_2%1)).astype(int)
+        blochvectors_expanded = self.blochvectors[i,j]
+        blochvectors = np.transpose(blochvectors_expanded, (2,3,0,1))
         
-                    
+        # Masking the data we do not want to plot
+        blochvectors[:,
+                     :,
+                     (kx>kxmax) | (kx<kxmin) | (ky>kymax) | (ky<kymin)] = np.nan
+        
         fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
         # Plot the surface.
-        surf = ax.plot_surface(self.kx, self.ky, gauge, cmap=cm.coolwarm,
-                            linewidth=0, antialiased=True)
+        surf = ax.plot_surface(kx, ky, blochvectors[band,component], 
+                               cmap=cm.coolwarm, linewidth=0)
+        ax.set_xlim(kxmin,kxmax)
+        ax.set_ylim(kymin,kymax)
         plt.savefig(save)
-        plt.show()        
-                                              
+        plt.show()  
 
-
-
-    def plot_bandstructure(self, save):
-        "Plots and saves the bandstructure." 
-        E = np.transpose(self.energies, (2,0,1))
+    def plot_bandstructure(self, 
+                           save, 
+                           kxmin=-np.pi, 
+                           kxmax=np.pi, 
+                           kymin=-np.pi, 
+                           kymax=np.pi):
+        "Plots and saves the bandstructure."
+        # Getting the correct region
+        span = False
+        reciprocal_vectors = 0
+        while not span:
+            reciprocal_vectors += 1
+            beta_1 = np.linspace(-reciprocal_vectors,
+                                 reciprocal_vectors,
+                                 2*reciprocal_vectors*self.n)
+            beta_2 = np.linspace(-reciprocal_vectors,
+                                 reciprocal_vectors,
+                                 2*reciprocal_vectors*self.n)
+            beta_1, beta_2 = np.meshgrid(beta_1, beta_2, indexing='ij')
+            kx = beta_1*self.b_1[0] + beta_2*self.b_2[0]
+            ky = beta_1*self.b_1[1] + beta_2*self.b_2[1]
+            span = ((np.min(kx) < kxmin) and (np.max(kx) > kxmax) 
+                    and (np.min(ky) < kymin) and (np.max(ky) > kymax))
+            
+        # Specifying the indices of the required energies in self.energies
+        i = (self.n*(beta_1%1)).astype(int)
+        j = (self.n*(beta_2%1)).astype(int)
+        energies_expanded = self.energies[i,j]
+        E = np.transpose(energies_expanded, (2,0,1))
+        
+        # Masking the data we do not want to plot
+        E[:, (kx>kxmax) | (kx<kxmin) | (ky>kymax) | (ky<kymin)] = np.nan
         
         fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
         # Plot the surface.
-        surf1 = ax.plot_surface(self.kx, self.ky, E[0], cmap=cm.coolwarm,
-                            linewidth=0, antialiased=True)
-        surf2 = ax.plot_surface(self.kx, self.ky, E[1], cmap=cm.coolwarm,
-                            linewidth=0, antialiased=True)
-        surf3 = ax.plot_surface(self.kx, self.ky, E[2], cmap=cm.coolwarm,
-                            linewidth=0, antialiased=True)
+        surf1 = ax.plot_surface(kx, ky, E[0], cmap=cm.coolwarm,
+                            linewidth=0)
+        surf2 = ax.plot_surface(kx, ky, E[1], cmap=cm.coolwarm,
+                            linewidth=0)
+        surf3 = ax.plot_surface(kx, ky, E[2], cmap=cm.coolwarm,
+                            linewidth=0)
+        ax.set_xlim(kxmin,kxmax)
+        ax.set_ylim(kymin,kymax)
         plt.savefig(save)
         plt.show()
 
@@ -136,7 +194,7 @@ class driven_bandstructure2D:
         ky = alpha_1*self.b_1[1] + alpha_2*self.b_2[1]
         energies = np.zeros(alpha_1.shape + (self.bands,), dtype='float')
         blochvectors = np.zeros(alpha_1.shape + (self.bands,self.bands), 
-                                dtype='float')
+                                dtype='complex')
         
         # Computing the right values for these grids
         for i in range(alpha_1.shape[0]):
@@ -146,7 +204,7 @@ class driven_bandstructure2D:
                     return self.hamiltonian(k,t)
                 U = compute_time_evolution_operator(H, self.T, self.dt)
                 eigenvalues, eigenvectors = np.linalg.eig(U)
-                eigenexp = np.real(np.log(eigenvalues) / (-1j*self.T))
+                eigenexp = np.real(np.log(eigenvalues) / (-1j))
                 # converting to the right quasienergy range
                 eigenexp = (eigenexp 
                             + 2*np.pi
@@ -167,42 +225,68 @@ class driven_bandstructure2D:
         self.ky = ky
         self.energies = energies
         self.blochvectors = blochvectors
-    
-    def plot_gauge(self, band_index, save):
-        """Visualises the gauge continuity over the BZ."""
-        gauge = np.zeros(self.alpha_1.shape, dtype='float')
-        for i in range(gauge.shape[0]):
-            for j in range(gauge.shape[1]):
-                gauge_indicator = (
-                    np.inner(self.blochvectors[i,j,band_index], 
-                            self.blochvectors[(i+1) % self.blochvectors.shape[0], 
-                                              j, 
-                                              band_index])
-                    + np.inner(self.blochvectors[i,j,band_index], 
-                            self.blochvectors[(i-1) % self.blochvectors.shape[0], 
-                                              j, 
-                                              band_index])
-                    + np.inner(self.blochvectors[i,j,band_index], 
-                            self.blochvectors[i, 
-                                              (j+1) % self.blochvectors.shape[1], 
-                                              band_index])
-                    + np.inner(self.blochvectors[i,j,band_index], 
-                            self.blochvectors[i, 
-                                              (j-1) % self.blochvectors.shape[1], 
-                                              band_index])) / 4
-                gauge[i,j] = gauge_indicator
+
+    def set_gauge(self):
+        for band in range(self.bands):
+            for i in range(self.blochvectors.shape[0]):
+                for j in range(self.blochvectors.shape[1]):
+                    if i == 0 and j == 0:
+                        pass
+                    elif i==0 and np.inner(self.blochvectors[i,j,band], 
+                                           self.blochvectors[i,j-1,band]) < 0:
+                        self.blochvectors[i,j,band] = -self.blochvectors[i,j,
+                                                                         band]
+                    elif np.inner(self.blochvectors[i,j,band], 
+                                  self.blochvectors[i-1,j,band]) < 0:
+                        self.blochvectors[i,j,band] = -self.blochvectors[i,j,
+                                                                         band]
+
+    def plot_blochvectors(self,
+                          band,
+                          component,
+                          save,
+                          kxmin=-np.pi,
+                          kxmax=np.pi,
+                          kymin=-np.pi,
+                          kymax=np.pi):
+        "Plots one component of the blochvectors."
+        # Getting the correct region
+        span = False
+        reciprocal_vectors = 0
+        while not span:
+            reciprocal_vectors += 1
+            beta_1 = np.linspace(-reciprocal_vectors,
+                                 reciprocal_vectors,
+                                 2*reciprocal_vectors*self.n)
+            beta_2 = np.linspace(-reciprocal_vectors,
+                                 reciprocal_vectors,
+                                 2*reciprocal_vectors*self.n)
+            beta_1, beta_2 = np.meshgrid(beta_1, beta_2, indexing='ij')
+            kx = beta_1*self.b_1[0] + beta_2*self.b_2[0]
+            ky = beta_1*self.b_1[1] + beta_2*self.b_2[1]
+            span = ((np.min(kx) < kxmin) and (np.max(kx) > kxmax) 
+                    and (np.min(ky) < kymin) and (np.max(ky) > kymax))
+            
+        # Specifying the indices of the required vectors in self.blochvectors
+        i = (self.n*(beta_1%1)).astype(int)
+        j = (self.n*(beta_2%1)).astype(int)
+        blochvectors_expanded = self.blochvectors[i,j]
+        blochvectors = np.transpose(blochvectors_expanded, (2,3,0,1))
         
-                    
+        # Masking the data we do not want to plot
+        blochvectors[:,
+                     :,
+                     (kx>kxmax) | (kx<kxmin) | (ky>kymax) | (ky<kymin)] = np.nan
+        
         fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
         # Plot the surface.
-        surf = ax.plot_surface(self.kx, self.ky, gauge, cmap=cm.coolwarm,
-                            linewidth=0, antialiased=True)
+        surf = ax.plot_surface(kx, ky, blochvectors[band,component], 
+                               cmap=cm.coolwarm, linewidth=0)
+        ax.set_xlim(kxmin,kxmax)
+        ax.set_ylim(kymin,kymax)
         plt.savefig(save)
-        plt.show()        
+        plt.show()       
                                               
-
-
-
     def plot_bandstructure(self, 
                            save, 
                            kxmin=-np.pi, 
