5d1b5193b000af53919c39e3b129dae6464e38ed
Laurens Smulders <64740743+laurenssmulders@users.noreply.github.com>
2023-11-16 17:08:47 +0000

    Zak phase code and plot design

--- execute.py
+++ execute.py
@@ -1,12 +1,17 @@
-from tight_binding.solver import static_bandstructure2D
-from tight_binding.hamiltonians import static_bloch_hamiltonian_kagome
+from tight_binding.solver import driven_bandstructure2D
+from tight_binding.hamiltonians import driven_bloch_hamiltonian_kagome
 import numpy as np
 
-H = static_bloch_hamiltonian_kagome(0,0,0,-1,1)
-bs = static_bandstructure2D(H,
+def A(t):
+    return np.array([2*np.cos(6*t), 0])
+
+H = driven_bloch_hamiltonian_kagome(0,3,-3,-1,1,A)
+bs = driven_bandstructure2D(H,
+                            2*np.pi / 6,
+                            0.01,
                             np.array([1,0]),
                             np.array([0.5, 0.5*3**0.5]),
-                            500,
-                            3)
-bs.compute_bandstructure()
+                            3,
+                            -1*np.pi)
+bs.compute_bandstructure(50)
 bs.plot_bandstructure('test.png', -1.25*np.pi, 1.25*np.pi, -1.25*np.pi, 1.25*np.pi)
--- tight_binding/solver.py
+++ tight_binding/solver.py
@@ -10,7 +10,6 @@ class static_bandstructure2D:
         hamiltonian,
         a_1: np.ndarray,
         a_2: np.ndarray,
-        n: int,
         bands: int=3  
     ):
         self.hamiltonian = hamiltonian
@@ -18,13 +17,24 @@ class static_bandstructure2D:
         self.a_2 = a_2
         self.b_1, self.b_2 = compute_reciprocal_lattice_vectors_2D(a_1,a_2)
         self.bands = bands
-        self.n = n
 
-    def compute_bandstructure(self):
+    def compute_energy_blochvector(self, k):
+        """Computes the quasienergies and blochvectors at a certain point."""
+        H = self.hamiltonian(k)
+        eigenvalues, eigenvectors = np.linalg.eig(H)
+        eigenvalues = np.real(eigenvalues) # H is hermitian
+        ind = np.argsort(eigenvalues) #sorting the energies
+        eigenvalues = eigenvalues[ind]
+        eigenvectors = eigenvectors[:,ind]
+        eigenvectors = eigenvectors.transpose(1,0)
+
+        return eigenvalues, eigenvectors
+
+    def compute_bandstructure(self, num_steps):
         """Computes the bandstructure for one reciprocal unit cell."""
         # Creating the required grids mapping from points in reciprocal space
-        alpha_1 = np.linspace(0,1,self.n)
-        alpha_2 = np.linspace(0,1,self.n)
+        alpha_1 = np.linspace(0,1,num_steps)
+        alpha_2 = np.linspace(0,1,num_steps)
         alpha_1, alpha_2 = np.meshgrid(alpha_1, alpha_2, indexing='ij')
         kx = alpha_1*self.b_1[0] + alpha_2*self.b_2[0]
         ky = alpha_1*self.b_1[1] + alpha_2*self.b_2[1]
@@ -36,15 +46,10 @@ class static_bandstructure2D:
         for i in range(alpha_1.shape[0]):
             for j in range(alpha_1.shape[1]):
                 k = np.array([kx[i,j], ky[i,j]])
-                H = self.hamiltonian(k)
-                eigenvalues, eigenvectors = np.linalg.eig(H)
-                eigenvalues = np.real(eigenvalues) # H is hermitian
-                ind = np.argsort(eigenvalues) #sorting the energies
-                eigenvalues = eigenvalues[ind]
-                eigenvectors = eigenvectors[:,ind]
+                eigenvalues, eigenvectors = self.compute_energy_blochvector(k)
                 for band in range(len(eigenvalues)):
                     energies[i,j,band] = eigenvalues[band]
-                    blochvectors[i,j,band] = eigenvectors[:,band]
+                    blochvectors[i,j,band] = eigenvectors[band]
         
         # Saving as class properties
         self.alpha_1 = alpha_1
@@ -85,10 +90,10 @@ class static_bandstructure2D:
             reciprocal_vectors += 1
             beta_1 = np.linspace(-reciprocal_vectors,
                                  reciprocal_vectors,
-                                 2*reciprocal_vectors*self.n)
+                                 2*reciprocal_vectors*self.alpha_1.shape[0])
             beta_2 = np.linspace(-reciprocal_vectors,
                                  reciprocal_vectors,
-                                 2*reciprocal_vectors*self.n)
+                                 2*reciprocal_vectors*self.alpha_1.shape[0])
             beta_1, beta_2 = np.meshgrid(beta_1, beta_2, indexing='ij')
             kx = beta_1*self.b_1[0] + beta_2*self.b_2[0]
             ky = beta_1*self.b_1[1] + beta_2*self.b_2[1]
@@ -96,8 +101,8 @@ class static_bandstructure2D:
                     and (np.min(ky) < kymin) and (np.max(ky) > kymax))
             
         # Specifying the indices of the required vectors in self.blochvectors
-        i = (self.n*(beta_1%1)).astype(int)
-        j = (self.n*(beta_2%1)).astype(int)
+        i = (self.alpha_1.shape[0]*(beta_1%1)).astype(int)
+        j = (self.alpha_1.shape[0]*(beta_2%1)).astype(int)
         blochvectors_expanded = self.blochvectors[i,j]
         blochvectors = np.transpose(blochvectors_expanded, (2,3,0,1))
         
@@ -129,10 +134,10 @@ class static_bandstructure2D:
             reciprocal_vectors += 1
             beta_1 = np.linspace(-reciprocal_vectors,
                                  reciprocal_vectors,
-                                 2*reciprocal_vectors*self.n)
+                                 2*reciprocal_vectors*self.alpha_1.shape[0])
             beta_2 = np.linspace(-reciprocal_vectors,
                                  reciprocal_vectors,
-                                 2*reciprocal_vectors*self.n)
+                                 2*reciprocal_vectors*self.alpha_1.shape[0])
             beta_1, beta_2 = np.meshgrid(beta_1, beta_2, indexing='ij')
             kx = beta_1*self.b_1[0] + beta_2*self.b_2[0]
             ky = beta_1*self.b_1[1] + beta_2*self.b_2[1]
@@ -140,8 +145,8 @@ class static_bandstructure2D:
                     and (np.min(ky) < kymin) and (np.max(ky) > kymax))
             
         # Specifying the indices of the required energies in self.energies
-        i = (self.n*(beta_1%1)).astype(int)
-        j = (self.n*(beta_2%1)).astype(int)
+        i = (self.alpha_1.shape[0]*(beta_1%1)).astype(int)
+        j = (self.alpha_1.shape[0]*(beta_2%1)).astype(int)
         energies_expanded = self.energies[i,j]
         E = np.transpose(energies_expanded, (2,0,1))
         
@@ -170,7 +175,6 @@ class driven_bandstructure2D:
         dt: float,
         a_1: np.ndarray,
         a_2: np.ndarray,
-        n: int,
         bands: int=3,
         lower_energy: float=-np.pi
     ):
@@ -179,16 +183,34 @@ class driven_bandstructure2D:
         self.a_2 = a_2
         self.b_1, self.b_2 = compute_reciprocal_lattice_vectors_2D(a_1,a_2)
         self.bands = bands
-        self.n = n
         self.T = period
         self.dt = dt
         self.lower_energy = lower_energy
 
-    def compute_bandstructure(self):
+    def compute_energy_blochvector(self, k):
+        """Computes the quasienergies and blochvectors at a certain point."""
+        def H(t):
+            return self.hamiltonian(k,t)
+        U = compute_time_evolution_operator(H, self.T, self.dt)
+        eigenvalues, eigenvectors = np.linalg.eig(U)
+        eigenexp = np.real(np.log(eigenvalues) / (-1j))
+        # converting to the right quasienergy range
+        eigenexp = (eigenexp 
+                    + 2*np.pi
+                    *np.floor(((self.lower_energy/np.pi 
+                                - eigenexp/np.pi) / 2
+                        + 1)))
+        ind = np.argsort(eigenexp) #sorting the energies
+        eigenexp = eigenexp[ind]
+        eigenvectors = eigenvectors[:,ind]
+        eigenvectors = eigenvectors.transpose(1,0)
+        return eigenexp, eigenvectors
+    
+    def compute_bandstructure(self, n_steps):
         """Computes the bandstructure for one reciprocal unit cell."""
         # Creating the required grids mapping from points in reciprocal space
-        alpha_1 = np.linspace(0,1,self.n)
-        alpha_2 = np.linspace(0,1,self.n)
+        alpha_1 = np.linspace(0,1,n_steps)
+        alpha_2 = np.linspace(0,1,n_steps)
         alpha_1, alpha_2 = np.meshgrid(alpha_1, alpha_2, indexing='ij')
         kx = alpha_1*self.b_1[0] + alpha_2*self.b_2[0]
         ky = alpha_1*self.b_1[1] + alpha_2*self.b_2[1]
@@ -200,23 +222,10 @@ class driven_bandstructure2D:
         for i in range(alpha_1.shape[0]):
             for j in range(alpha_1.shape[1]):
                 k = np.array([kx[i,j], ky[i,j]])
-                def H(t):
-                    return self.hamiltonian(k,t)
-                U = compute_time_evolution_operator(H, self.T, self.dt)
-                eigenvalues, eigenvectors = np.linalg.eig(U)
-                eigenexp = np.real(np.log(eigenvalues) / (-1j))
-                # converting to the right quasienergy range
-                eigenexp = (eigenexp 
-                            + 2*np.pi
-                            *np.floor(((self.lower_energy/np.pi 
-                                        - eigenexp/np.pi) / 2
-                              + 1)))
-                ind = np.argsort(eigenexp) #sorting the energies
-                eigenexp = eigenexp[ind]
-                eigenvectors = eigenvectors[:,ind]
+                eigenexp, eigenvectors = self.compute_energy_blochvector(k)
                 for band in range(len(eigenexp)):
                     energies[i,j,band] = eigenexp[band]
-                    blochvectors[i,j,band] = eigenvectors[:,band]
+                    blochvectors[i,j,band] = eigenvectors[band]
         
         # Saving as class properties
         self.alpha_1 = alpha_1
@@ -225,6 +234,33 @@ class driven_bandstructure2D:
         self.ky = ky
         self.energies = energies
         self.blochvectors = blochvectors
+
+    def calculate_zak_phase(self, 
+                            direction, 
+                            band, 
+                            k_start, 
+                            path_length, 
+                            n_steps):
+        """Calculates the zak phase along a given direction for a given band."""
+        path_steps = np.linspace(0,1,n_steps,endpoint=False)
+        path_k = np.zeros([len(path_steps), 2])
+        for i in path_k.shape[0]:
+            path_k[i] = (k_start + path_length * path_steps[i]
+                         * direction / np.linalg.norm(direction))
+        path_blochvectors = np.zeros([len(path_steps), self.bands])
+        for i in range(len(path_steps)):
+            _, path_blochvectors[i] = self.compute_energy_blochvector(path_k[i])
+        zak_phase = 1 + 0j
+        for i in range(len(path_steps)):
+            zak_phase = (zak_phase 
+                         * np.inner(path_blochvectors[i], 
+                                    path_blochvectors[(i+1) 
+                                                      % len(path_steps)]))
+        zak_phase = 1j*np.log(zak_phase)
+        return zak_phase
+        
+
+
 
     def set_gauge(self):
         for band in range(self.bands):
@@ -257,10 +293,10 @@ class driven_bandstructure2D:
             reciprocal_vectors += 1
             beta_1 = np.linspace(-reciprocal_vectors,
                                  reciprocal_vectors,
-                                 2*reciprocal_vectors*self.n)
+                                 2*reciprocal_vectors*self.alpha_1.shape[0])
             beta_2 = np.linspace(-reciprocal_vectors,
                                  reciprocal_vectors,
-                                 2*reciprocal_vectors*self.n)
+                                 2*reciprocal_vectors*self.alpha_1.shape[0])
             beta_1, beta_2 = np.meshgrid(beta_1, beta_2, indexing='ij')
             kx = beta_1*self.b_1[0] + beta_2*self.b_2[0]
             ky = beta_1*self.b_1[1] + beta_2*self.b_2[1]
@@ -268,8 +304,8 @@ class driven_bandstructure2D:
                     and (np.min(ky) < kymin) and (np.max(ky) > kymax))
             
         # Specifying the indices of the required vectors in self.blochvectors
-        i = (self.n*(beta_1%1)).astype(int)
-        j = (self.n*(beta_2%1)).astype(int)
+        i = (self.alpha_1.shape[0]*(beta_1%1)).astype(int)
+        j = (self.alpha_1.shape[0]*(beta_2%1)).astype(int)
         blochvectors_expanded = self.blochvectors[i,j]
         blochvectors = np.transpose(blochvectors_expanded, (2,3,0,1))
         
@@ -301,18 +337,18 @@ class driven_bandstructure2D:
             reciprocal_vectors += 1
             beta_1 = np.linspace(-reciprocal_vectors,
                                  reciprocal_vectors,
-                                 2*reciprocal_vectors*self.n)
+                                 2*reciprocal_vectors*self.alpha_1.shape[0])
             beta_2 = np.linspace(-reciprocal_vectors,
                                  reciprocal_vectors,
-                                 2*reciprocal_vectors*self.n)
+                                 2*reciprocal_vectors*self.alpha_1.shape[0])
             beta_1, beta_2 = np.meshgrid(beta_1, beta_2, indexing='ij')
             kx = beta_1*self.b_1[0] + beta_2*self.b_2[0]
             ky = beta_1*self.b_1[1] + beta_2*self.b_2[1]
             span = ((np.min(kx) < kxmin) and (np.max(kx) > kxmax) 
                     and (np.min(ky) < kymin) and (np.max(ky) > kymax))
         # Specifying the indices of the required energies in self.energies
-        i = (self.n*(beta_1%1)).astype(int)
-        j = (self.n*(beta_2%1)).astype(int)
+        i = (self.alpha_1.shape[0]*(beta_1%1)).astype(int)
+        j = (self.alpha_1.shape[0]*(beta_2%1)).astype(int)
         energies_expanded = self.energies[i,j]
         E = np.transpose(energies_expanded, (2,0,1))
         
@@ -327,6 +363,15 @@ class driven_bandstructure2D:
                             linewidth=0)
         surf3 = ax.plot_surface(kx, ky, E[2], cmap=cm.coolwarm,
                             linewidth=0)
+        tick_values = np.linspace(-4,4,9) * np.pi / 2
+        tick_labels = ['$-2\pi$', '', '$-\pi$', '', '0', '', '$\pi$', '', '$2\pi$']
+        ax.set_xticks(tick_values)
+        ax.set_xticklabels(tick_labels)
+        ax.set_yticks(tick_values)
+        ax.set_yticklabels(tick_labels)
+        ax.set_zticks(tick_values)
+        ax.set_zticklabels(tick_labels)
+        ax.set_zlim(self.lower_energy, self.lower_energy + 2*np.pi)
         ax.set_xlim(kxmin,kxmax)
         ax.set_ylim(kymin,kymax)
         plt.savefig(save)
