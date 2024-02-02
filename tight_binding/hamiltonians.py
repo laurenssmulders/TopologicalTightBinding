b2b385aeab4995f3f53131a25abed126e789d484
laurenssmulders <laurenssmulders21@gmail.com>
2024-02-02 14:20:41 +0000

    Ortho diag tunneling distinction for square

--- tight_binding/hamiltonians.py
+++ tight_binding/hamiltonians.py
@@ -66,12 +66,18 @@ def square_hamiltonian_static(delta_a=0,
                               J_ab_1y=0, 
                               J_ac_1y=0, 
                               J_bc_1y=0,
-                              J_aa_2=0, 
-                              J_bb_2=0, 
-                              J_cc_2=0, 
-                              J_ab_2=0, 
-                              J_ac_2=0, 
-                              J_bc_2=0,
+                              J_aa_2p=0, 
+                              J_bb_2p=0, 
+                              J_cc_2p=0, 
+                              J_ab_2p=0, 
+                              J_ac_2p=0, 
+                              J_bc_2p=0,
+                              J_aa_2m=0, 
+                              J_bb_2m=0, 
+                              J_cc_2m=0, 
+                              J_ab_2m=0, 
+                              J_ac_2m=0, 
+                              J_bc_2m=0,
                               a=1):
     """Defines a bloch hamiltonian for the out-of-equillibrium square lattice.
     
@@ -122,29 +128,42 @@ def square_hamiltonian_static(delta_a=0,
     J_bc_1y: float
         The tunneling parameter between nn B and C orbitals in the vertical 
         direction
-    J_aa_2: float
-        The tunneling parameter between nn A orbitals in the diagonal direction
-    J_bb_2: float
-        The tunneling parameter between nn B orbitals in the diagonal direction
-    J_cc_2: float
-        The tunneling parameter between nn C orbitals in the diagonal direction
-    J_ab_2: float
+    J_aa_2p: float
+        The tunneling parameter between nn A orbitals in the diagonal a1+a2 
+        direction
+    J_bb_2p: float
+        The tunneling parameter between nn B orbitals in the diagonal a1+a2 
+        direction
+    J_cc_2p: float
+        The tunneling parameter between nn C orbitals in the diagonal a1+a2 
+        direction
+    J_ab_2p: float
         The tunneling parameter between nn A and B orbitals in the diagonal 
-        direction
-    J_ac_2: float
+        a1+a2 direction
+    J_ac_2p: float
         The tunneling parameter between nn A and C orbitals in the diagonal 
-        direction
-    J_bc_2: float
+        a1+a2 direction
+    J_bc_2p: float
         The tunneling parameter between nn B and C orbitals in the diagonal 
+        a1+a2 direction
+    J_aa_2m: float
+        The tunneling parameter between nn A orbitals in the diagonal a1-a2 
         direction
-    A_x: float
-        The amplitude of the x component of the driving vector potential
-    A_y: float
-        The amplitude of the y component of the driving vector potential
-    omega: float
-        The angular frequency of the driving vector potential
-    phi:
-        The relative phase of the phi component of the vector potential
+    J_bb_2m: float
+        The tunneling parameter between nn B orbitals in the diagonal a1-a2 
+        direction
+    J_cc_2m: float
+        The tunneling parameter between nn C orbitals in the diagonal a1-a2 
+        direction
+    J_ab_2m: float
+        The tunneling parameter between nn A and B orbitals in the diagonal 
+        a1-a2 direction
+    J_ac_2m: float
+        The tunneling parameter between nn A and C orbitals in the diagonal 
+        a1-a2 direction
+    J_bc_2m: float
+        The tunneling parameter between nn B and C orbitals in the diagonal 
+        a1-a2 direction
     a: float
         The lattice spacing
 
@@ -165,9 +184,12 @@ def square_hamiltonian_static(delta_a=0,
     J_ba_1y = np.conjugate(J_ab_1y)
     J_ca_1y = np.conjugate(J_ac_1y)
     J_cb_1y = np.conjugate(J_bc_1y)
-    J_ba_2 = np.conjugate(J_ab_2)
-    J_ca_2 = np.conjugate(J_ac_2)
-    J_cb_2 = np.conjugate(J_bc_2)
+    J_ba_2p = np.conjugate(J_ab_2p)
+    J_ca_2p = np.conjugate(J_ac_2p)
+    J_cb_2p = np.conjugate(J_bc_2p)
+    J_ba_2m = np.conjugate(J_ab_2m)
+    J_ca_2m = np.conjugate(J_ac_2m)
+    J_cb_2m = np.conjugate(J_bc_2m)
     
     def H(k):
        H_onsite = np.array([
@@ -188,14 +210,20 @@ def square_hamiltonian_static(delta_a=0,
            [J_ca_1y, J_cb_1y, J_cc_1y]
        ]) * -2 * np.cos(np.vdot(k,a_2))
 
-       H_diagonal = np.array([
-           [J_aa_2, J_ab_2, J_ac_2],
-           [J_ba_2, J_bb_2, J_bc_2],
-           [J_ca_2, J_cb_2, J_cc_2]
-       ]) * -2 * (np.cos(np.vdot(k,a_1 + a_2)) 
-                  + np.cos(np.vdot(k,a_1 - a_2)))
+       H_diagonal_p = np.array([
+           [J_aa_2p, J_ab_2p, J_ac_2p],
+           [J_ba_2p, J_bb_2p, J_bc_2p],
+           [J_ca_2p, J_cb_2p, J_cc_2p]
+       ]) * -2 * np.cos(np.vdot(k,a_1 + a_2))
+
+       H_diagonal_m = np.array([
+           [J_aa_2m, J_ab_2m, J_ac_2m],
+           [J_ba_2m, J_bb_2m, J_bc_2m],
+           [J_ca_2m, J_cb_2m, J_cc_2m]
+       ]) * -2 * np.cos(np.vdot(k,a_1 - a_2))
        
-       hamiltonian = H_onsite + H_horizontal + H_vertical + H_diagonal
+       hamiltonian = (H_onsite + H_horizontal + H_vertical + H_diagonal_p 
+                      + H_diagonal_m)
        return hamiltonian
     
     return H
@@ -282,12 +310,18 @@ def square_hamiltonian_driven(delta_a=0,
                               J_ab_1y=0, 
                               J_ac_1y=0, 
                               J_bc_1y=0,
-                              J_aa_2=0, 
-                              J_bb_2=0, 
-                              J_cc_2=0, 
-                              J_ab_2=0, 
-                              J_ac_2=0, 
-                              J_bc_2=0,  
+                              J_aa_2p=0, 
+                              J_bb_2p=0, 
+                              J_cc_2p=0, 
+                              J_ab_2p=0, 
+                              J_ac_2p=0, 
+                              J_bc_2p=0,
+                              J_aa_2m=0, 
+                              J_bb_2m=0, 
+                              J_cc_2m=0, 
+                              J_ab_2m=0, 
+                              J_ac_2m=0, 
+                              J_bc_2m=0,  
                               A_x=0, 
                               A_y=0, 
                               omega=0, 
@@ -342,21 +376,42 @@ def square_hamiltonian_driven(delta_a=0,
     J_bc_1y: float
         The tunneling parameter between nn B and C orbitals in the vertical 
         direction
-    J_aa_2: float
-        The tunneling parameter between nn A orbitals in the diagonal direction
-    J_bb_2: float
-        The tunneling parameter between nn B orbitals in the diagonal direction
-    J_cc_2: float
-        The tunneling parameter between nn C orbitals in the diagonal direction
-    J_ab_2: float
+    J_aa_2p: float
+        The tunneling parameter between nn A orbitals in the diagonal a1+a2 
+        direction
+    J_bb_2p: float
+        The tunneling parameter between nn B orbitals in the diagonal a1+a2 
+        direction
+    J_cc_2p: float
+        The tunneling parameter between nn C orbitals in the diagonal a1+a2 
+        direction
+    J_ab_2p: float
         The tunneling parameter between nn A and B orbitals in the diagonal 
-        direction
-    J_ac_2: float
+        a1+a2 direction
+    J_ac_2p: float
         The tunneling parameter between nn A and C orbitals in the diagonal 
-        direction
-    J_bc_2: float
+        a1+a2 direction
+    J_bc_2p: float
         The tunneling parameter between nn B and C orbitals in the diagonal 
+        a1+a2 direction
+    J_aa_2m: float
+        The tunneling parameter between nn A orbitals in the diagonal a1-a2 
         direction
+    J_bb_2m: float
+        The tunneling parameter between nn B orbitals in the diagonal a1-a2 
+        direction
+    J_cc_2m: float
+        The tunneling parameter between nn C orbitals in the diagonal a1-a2 
+        direction
+    J_ab_2m: float
+        The tunneling parameter between nn A and B orbitals in the diagonal 
+        a1-a2 direction
+    J_ac_2m: float
+        The tunneling parameter between nn A and C orbitals in the diagonal 
+        a1-a2 direction
+    J_bc_2m: float
+        The tunneling parameter between nn B and C orbitals in the diagonal 
+        a1-a2 direction
     A_x: float
         The amplitude of the x component of the driving vector potential
     A_y: float
@@ -385,9 +440,12 @@ def square_hamiltonian_driven(delta_a=0,
     J_ba_1y = np.conjugate(J_ab_1y)
     J_ca_1y = np.conjugate(J_ac_1y)
     J_cb_1y = np.conjugate(J_bc_1y)
-    J_ba_2 = np.conjugate(J_ab_2)
-    J_ca_2 = np.conjugate(J_ac_2)
-    J_cb_2 = np.conjugate(J_bc_2)
+    J_ba_2p = np.conjugate(J_ab_2p)
+    J_ca_2p = np.conjugate(J_ac_2p)
+    J_cb_2p = np.conjugate(J_bc_2p)
+    J_ba_2m = np.conjugate(J_ab_2m)
+    J_ca_2m = np.conjugate(J_ac_2m)
+    J_cb_2m = np.conjugate(J_bc_2m)
 
     def A(time):
         drive = np.array([A_x*np.cos(omega*time), 
@@ -413,14 +471,20 @@ def square_hamiltonian_driven(delta_a=0,
            [J_ca_1y, J_cb_1y, J_cc_1y]
        ]) * -2 * np.cos(np.vdot((k+A(t)),a_2))
 
-       H_diagonal = np.array([
-           [J_aa_2, J_ab_2, J_ac_2],
-           [J_ba_2, J_bb_2, J_bc_2],
-           [J_ca_2, J_cb_2, J_cc_2]
-       ]) * -2 * (np.cos(np.vdot((k+A(t)),a_1 + a_2)) 
-                  + np.cos(np.vdot((k+A(t)),a_1 - a_2)))
+       H_diagonal_p = np.array([
+           [J_aa_2p, J_ab_2p, J_ac_2p],
+           [J_ba_2p, J_bb_2p, J_bc_2p],
+           [J_ca_2p, J_cb_2p, J_cc_2p]
+       ]) * -2 * np.cos(np.vdot((k+A(t)),a_1 + a_2))
+
+       H_diagonal_m = np.array([
+           [J_aa_2m, J_ab_2m, J_ac_2m],
+           [J_ba_2m, J_bb_2m, J_bc_2m],
+           [J_ca_2m, J_cb_2m, J_cc_2m]
+       ]) * -2 * np.cos(np.vdot((k+A(t)),a_1 + a_2))
        
-       hamiltonian = H_onsite + H_horizontal + H_vertical + H_diagonal
+       hamiltonian = (H_onsite + H_horizontal + H_vertical + H_diagonal_p + 
+                      H_diagonal_m)
        return hamiltonian
     
     return H
