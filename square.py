329544945d3cd458d53435c3bdfa657811249105
Laurens Smulders <64740743+laurenssmulders@users.noreply.github.com>
2024-02-04 12:45:00 +0000

    test

--- square.py
+++ square.py
@@ -27,53 +27,44 @@ lowest_quasi_energy = -np.pi
 
 # BLOCH HAMILTONIAN
 H = square_hamiltonian_driven(
-    delta_aa=delta_A,
-    delta_bb=delta_B,
-    delta_cc=delta_C,
-    J_bb_1x=J_1,
-    J_bc_1x=J_1,
-    J_cc_1x=J_1,
-    J_aa_1y=J_1,
-    J_ac_1y=J_1,
-    J_cc_1y=J_1,
-    J_aa_2=J_2,
-    J_ab_2=J_2,
-    J_ac_2=J_2,
-    J_bb_2=J_2,
-    J_bc_2=J_2,
-    J_cc_2=J_2,
-    A_x=A_x,
-    omega=omega
+    delta_aa=0,
+    delta_bb=0,
+    delta_cc=0,
+    delta_ab=1,
+    delta_ac=1,
+    delta_bc=1,
+    J_bc_1x=1,
+    J_ac_1y=1
 )
 
 # BANDSTRUCTURE
-#energies, blochvectors = compute_bandstructure2D(H, a_1, a_2, num_points, omega,
-#                                                 num_steps, lowest_quasi_energy)
+energies, blochvectors = compute_bandstructure2D(H, a_1, a_2, num_points, omega,
+                                                 num_steps, lowest_quasi_energy)
 
-#plot_bandstructure2D(energies, a_1, a_2, 'test.png', bands_to_plot = [1,1,1], 
-#                     lowest_quasi_energy=lowest_quasi_energy, 
-#                     discontinuity_threshold = 0.05)
+plot_bandstructure2D(energies, a_1, a_2, 'test.png', bands_to_plot = [1,1,1], 
+                     lowest_quasi_energy=lowest_quasi_energy, 
+                     discontinuity_threshold = 0.05)
 
 # ZAK PHASES
 
-for i in range(100):
-    start = np.array([0,i / 100])
-    end = np.array([1,i / 100])
-    k = i / 100 * 2
+#for i in range(100):
+#    start = np.array([0,i / 100])
+#    end = np.array([1,i / 100])
+#    k = i / 100 * 2
 
-    offsets = np.zeros((3,2))
-    zak_phase, energies = compute_zak_phase(H, a_1, a_2, offsets, start, end, num_points, 
-                                omega, num_steps, lowest_quasi_energy)
-    zak_phase = np.rint(np.real(zak_phase / np.pi))
+#    offsets = np.zeros((3,2))
+#    zak_phase, energies = compute_zak_phase(H, a_1, a_2, offsets, start, end, num_points, 
+#                                omega, num_steps, lowest_quasi_energy)
+#    zak_phase = np.rint(np.real(zak_phase / np.pi))
 
-    fig = plt.figure()
-    plt.plot(energies[:,0], label='band 0')
-    plt.plot(energies[:,1], label='band 1')
-    plt.plot(energies[:,2], label='band 2')
-    plt.legend()
-    plt.title('$k_y  = {k}\pi$ Zak Phases: {zak_phase}'.format(k=k, zak_phase=zak_phase))
-    plt.savefig('figures/square/square_Ax_J2_dC_1p5_0p8_0p0_yslices/square_Ax_J2_dC_1p5_0p0_0p5_yslice{i}'.format(i=i))
-    plt.close()
+#    fig = plt.figure()
+#    plt.plot(energies[:,0], label='band 0')
+#    plt.plot(energies[:,1], label='band 1')
+#    plt.plot(energies[:,2], label='band 2')
+#    plt.legend()
+#    plt.title('$k_y  = {k}\pi$ Zak Phases: {zak_phase}'.format(k=k, zak_phase=zak_phase))
+#    plt.savefig('figures/square/square_Ax_J2_dC_1p5_0p8_0p0_yslices/square_Ax_J2_dC_1p5_0p0_0p5_yslice{i}'.format(i=i))
+#    plt.close()
 
 # DIRAC STRINGS
 
