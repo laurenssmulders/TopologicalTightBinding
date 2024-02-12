7f15b97376dc116f09ccdb24ac9ae3beec1a591a
Laurens Smulders <laurenssmulders21@gmail.com>
2024-02-12 15:12:52 +0000

    SP3

--- square_sp3_driven_scan.py
+++ square_sp3_driven_scan.py
@@ -30,8 +30,8 @@ perpendicular_directions = np.array([
 ])
 
 # PARAMETERS
-delta_A_scan = np.array([3.1,3.6,4.1])
-delta_C_scan = np.array([-3.1,-3.6,-4.1])
+delta_A_scan = np.array([2.6,2.1,1.6,1.1,0.6,0.1,-0.4])
+#delta_C_scan = np.array([-3.1,-3.6,-4.1])
 omega = 10
 A_x = 1
 
@@ -47,9 +47,9 @@ offsets = np.zeros((3,2))
 
 for i in range(len(delta_A_scan)):
     print(i)
-    for j in range(len(delta_C_scan)):
+    for j in range(1):
         delta_A = delta_A_scan[i]
-        delta_C = delta_C_scan[j]
+        delta_C = delta_A - 7.2
         delta_B = - delta_A - delta_C
 
         name = 'SP3_driven_Ax_w_dA_dC_{A_x}_{omega}_{delta_A}_{delta_C}'.format(
