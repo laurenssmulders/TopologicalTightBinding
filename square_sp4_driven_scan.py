22cfea8a7841329980e5bfd994268b0b89f4d9be
Laurens Smulders <laurenssmulders21@gmail.com>
2024-02-14 15:50:18 +0000

    SP4

--- square_sp4_driven_scan.py
+++ square_sp4_driven_scan.py
@@ -31,8 +31,8 @@ perpendicular_directions = np.array([
 
 # PARAMETERS
 delta_A_scan = np.array([0])
-delta_C_scan = np.array([0,-0.5])
-delta_A_centre = 0
+delta_C_scan = np.array([0])
+delta_A_centre = -0.5
 delta_C_centre = -2.0
 omega = 11
 A_x = 1
