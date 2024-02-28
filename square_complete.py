08fc1763abe6a90ebb6a71f6017b061d84119ac5
Laurens Smulders <laurenssmulders21@gmail.com>
2024-02-28 19:55:44 +0000

    Braiding

--- square_complete.py
+++ square_complete.py
@@ -6,7 +6,7 @@ from tight_binding.bandstructure import compute_bandstructure2D, plot_bandstruct
 from tight_binding.topology import compute_zak_phase, compute_patch_euler_class
 from tight_binding.diagonalise import compute_eigenstates
 
-plotting = False
+plotting = True
 slicing = False
 zak = False
 patch_euler_class = False
@@ -16,8 +16,8 @@ plot_from_save = True
 
 
 # PARAMETERS
-delta_A = -0.5
-delta_C = -0.5
+delta_A = -1.5
+delta_C = -1.5
 omega = 11
 A_x = 1
 A_y = 1
@@ -40,10 +40,10 @@ start = np.array([0,0])
 end = np.array([1,0])
 
 ### Euler class parameters
-kxmin= -1
-kxmax= 1
-kymin = -1
-kymax = 1
+kxmin= 0
+kxmax= np.pi
+kymin = 0
+kymax = np.pi
 bands = [0,1]
 
 ### Finite geometry parameters
